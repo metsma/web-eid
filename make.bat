0bd10f2a8df24e062d7fa27034d135ec4256a6ef
Martin Paljak <martin@martinpaljak.net>
2016-12-27 16:24:37 +0200

    Better "make" imitation, that uses VS2015

--- make.bat
+++ make.bat
@@ -1,2 +1,6 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
+WHERE /q nmake.exe
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO "No Visual Studio environment found, loading VS 2015"
+  CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
+)
 nmake %1
