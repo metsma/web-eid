b648035d296ff9eae551715ae57648df033b80bd
Martin Paljak <martin@martinpaljak.net>
2017-02-19 16:12:31 +0200

    build: pass all parameters of nmake wrapper to nmake

--- make.bat
+++ make.bat
@@ -3,4 +3,4 @@ IF %ERRORLEVEL% NEQ 0 (
   ECHO "No Visual Studio environment found, loading VS 2015"
   CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
 )
-nmake %1
+nmake %*
