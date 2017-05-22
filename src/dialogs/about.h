cb343bfb97a62e16442fa5fbd9dd50363454e03f
Martin Paljak <martin@martinpaljak.net>
2017-05-22 07:40:23 +0300

    better about

--- src/dialogs/about.h
+++ src/dialogs/about.h
@@ -6,6 +6,7 @@
 
 #include "dialogs/betterdialog.h"
 #include <QLabel>
+#include <QIcon>
 #include <QVBoxLayout>
 #include <QDialogButtonBox>
 
@@ -25,15 +26,16 @@ public:
         layout->addWidget(text);
         layout->addWidget(buttons);
 
-        setWindowTitle("About Web eID app");
+        setWindowTitle(tr("About"));
+        setWindowIcon(QIcon());
         setWindowFlags(Qt::WindowStaysOnTopHint);
         setWindowFlags((windowFlags()|Qt::CustomizeWindowHint) &
                        ~(Qt::WindowMaximizeButtonHint|Qt::WindowMinimizeButtonHint|Qt::WindowCloseButtonHint));
         setAttribute(Qt::WA_DeleteOnClose);
-        // remove minimize and maximize buttons
+
         buttons->addButton(tr("OK"), QDialogButtonBox::AcceptRole);
         connect(buttons, &QDialogButtonBox::accepted, this, &QDialog::accept);
-        text->setText(tr("<h3>Web eID v%1</h3><div>Use your eID on the Web!</div><p>&copy; 2017 <a href=\"mailto:martin@martinpaljak.net\">Martin Paljak</a></p><p>More information on <a href=\"https://web-eid.com\">web-eid.com</a></p>").arg(VERSION));
+        text->setText(tr("<img src=\":/web-eid.png\"><h3>Web eID v%1</h3><div>Use your eID smart card on the Web!</div><p>&copy; 2017 <a href=\"mailto:martin@martinpaljak.net\">Martin Paljak</a> & contributors</p><p>More information on <a href=\"https://web-eid.com\">web-eid.com</a></p>").arg(VERSION));
         text->setAlignment(Qt::AlignCenter);
         text->setTextFormat(Qt::RichText);
         //text->setTextInteractionFlags(Qt::TextBrowserInteraction);
